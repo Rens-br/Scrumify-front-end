7cc78ece9b2609f34a86f4e3a244bbdb7bf16c4d
Brekel,Rens R. van den <r.vandenbrekel@student.fontys.nl>
2019-12-04 14:09:47 +0100

    Add basic boards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import SideBar from './components/SideBar';
+import Board from "./components/Board";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
       <SideBar />
+      <Content/>
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Content.css'
+import Board from "./Board";
+import { Component } from 'react';
+import {inject, observer} from 'mobx-react';
+
+const Content = inject('store')(observer(class Content extends Component{
+    render() {
+        return (
+            <div id='content'>
+                <Board sprint={this.props.store.projectStore.sprints[0]}/>
+            </div>
+        );
+    }
+}));
+
+export default Content;
