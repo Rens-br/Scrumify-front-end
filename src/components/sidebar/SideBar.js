1f72841c4f893777eac0eb7060b4757dc90142cc
Daan Brocatus <daan.brocatus@outlook.com>
2019-12-10 11:12:52 +0100

    Edit sidebar some more to add conditional rendering state

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -10,11 +10,29 @@ import ProjectList from './ProjectList';
 export default class SideBar extends React.Component{
   constructor(props){
     super(props);
-    this.state = 
+    this.state = {
+      isDesktop: false
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+
+  componentDidMount(){
+    this.updatePredicate();
+    window.addEventListener("resize" , this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate(){
+    this.setState({ isDesktop: window.innerWidth > 1450 });
   }
 
   render(){
+    const isDesktop = this.state.isDesktop;
     return(
+      {isDesktop && (
       <div id="sidebar">
         <div className="topbar">
           <Header/>
@@ -26,6 +44,7 @@ export default class SideBar extends React.Component{
           <ProjectList/>
         </div>
       </div>
+      )}
     );
   }
 }
