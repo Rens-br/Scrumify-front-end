0c4fbdb92ddeb30c2d744f3c3406485e8640b05d
Daan Brocatus <daan.brocatus@outlook.com>
2019-12-16 14:29:56 +0100

    Do some more work on organization dropdown, still not selecting org properly.

--- src/components/CustomDropDown.js
+++ src/components/CustomDropDown.js
@@ -3,36 +3,40 @@ import { Component } from 'react'
 import './CustomDropDown.css'
 import MaterialIcon from '@material/react-material-icon';
 import DropdownItem from './DropdownItem';
+import { inject, observer } from 'mobx-react';
 
-class CustomDropDown extends Component{
+const CustomDropDown = inject('store')(observer(class CustomDropDown extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
 			dropdownToggled: false
 		}
+		this.onSelect = this.onSelect.bind(this);
 	}
 
 	toggleDropdown = () => {
-		this.setState({dropdownToggled: !this.state.dropdownToggled})
-		console.log(this.state.dropdownToggled);
+		this.setState({dropdownToggled: !this.state.dropdownToggled});
+	}
+
+	onSelect(id) {
+		this.props.store.userStore.setCurrentOrganization(id);
 	}
 
 	render() {
 		return (
 			<div className='customDropDown' onClick={this.toggleDropdown}>
-				<p>Company</p>
+				{/* TODO: fix selecting organizations from dropdown */}
+				<DropdownItem label={this.props.store.userStore.currentOrganization} />
 				<MaterialIcon icon='keyboard_arrow_down'/>
 				{this.state.dropdownToggled && (
 					<div className="extendedDropdown">
-						<DropdownItem className="ddItem" label="Testing123"/>
-						<DropdownItem className="ddItem" label="kkRens"/>
-						<DropdownItem className="ddItem" label="Rens sloopt alles"/>
-						<DropdownItem className="ddItem" label="!ez4rens"/>
+				{this.props.store.userStore.organizations.map((obj) => <DropdownItem className="ddItem" label={obj.name} key={obj.id} onClick={this.props.store.userStore.setCurrentOrganization(obj.id)}/>)}
+						<DropdownItem onClick={this.props.store.userStore.setCurrentOrganization(null)} label="Empty" key={0}/>
 					</div>
 				)}
 			</div>
 		);
 	}
-}
+}));
 
 export default CustomDropDown;
--- src/components/DropdownItem.js
+++ src/components/DropdownItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../css/bootstrap.min.css';
 import './DropdownItem.css';
-import MaterialIcon from '@material/react-material-icon';
 
 
 export default class DropdownItem extends React.Component{
   render(){
     return(
         <div className="ddItemContainer">
-            <p>{this.props.label}</p>
+            <p className="ddItem" key={this.props.id} >{this.props.label}</p>
         </div>
     );
   }
