7b61edf5802ac9190b5c111e47544a3eb30a50a7
Daan Brocatus <daan.brocatus@outlook.com>
2019-12-18 13:01:00 +0100

    Style login screen.

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -35,36 +35,45 @@ const LoginScreen = () => (
                 handleSubmit
             } = props;
             return (
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        name="email"
-                        type="text"
-                        placeholder="Enter your email"
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className={errors.email && touched.email && "error"}
-                    />
-                    {errors.email && touched.email && (
-                        <div className="input-feedback">{errors.email}</div>
-                    )}
-                    <label htmlFor="email">Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className={errors.password && touched.password && "error"}
-                    />
-                    {errors.password && touched.password && (
-                        <div className="input-feedback">{errors.password}</div>
-                    )}
-                    <button type="submit" disabled={isSubmitting}>
+                <form onSubmit={handleSubmit} className="loginForm">
+                    <div className="header">
+                        <h1>Welcome</h1>
+                        <p>Login to gain access to all your boards and projects.</p>
+                    </div>
+                    {/* <label htmlFor="email">Email</label> */}
+                    <div id="inputField">
+                        <input
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={errors.email && touched.email ? "error inputTb" : "inputTb"}
+                        />
+                        {errors.email && touched.email && (
+                            <div className="input-feedback">{errors.email}</div>
+                        )}
+                    </div>
+                    {/* <label htmlFor="email">Password</label> */}
+                    <div id="inputField">
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={errors.password && touched.password ? "error inputTb" : "inputTb"}
+                        />
+                        {errors.password && touched.password && (
+                            <div className="input-feedback">{errors.password}</div>
+                        )}
+                    </div>
+                    <button className="loginBtn" type="submit" disabled={isSubmitting}>
                         Login
                     </button>
+                    <a></a>
                 </form>
             );
         }}
