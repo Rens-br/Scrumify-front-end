3aa3195c8df417fb989fc5451d84392064c2c048
Daan Brocatus <daan.brocatus@outlook.com>
2020-01-06 09:10:54 +0100

    Style login screen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -73,7 +73,6 @@ const LoginScreen = () => (
                     <button className="loginBtn" type="submit" disabled={isSubmitting}>
                         Login
                     </button>
-                    <a></a>
                 </form>
             );
         }}
