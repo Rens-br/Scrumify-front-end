08862d1ea38c8bf2d7084da2e2091efce52858e3
Daan Brocatus <daan.brocatus@outlook.com>
2020-01-09 15:58:08 +0100

    Style organization screen

--- src/components/OrganizationScreen.js
+++ src/components/OrganizationScreen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CustomInputField from "./CustomInputField";
 import CustomButton from "./CustomButton";
 import {inject, observer} from "mobx-react";
+import './OrganizationScreen.css';
 
 const OrganizationScreen = inject('store')(observer(class OrganizationScreen extends Component {
     constructor(props) {
@@ -20,11 +21,11 @@ const OrganizationScreen = inject('store')(observer(class OrganizationScreen ext
 
     render() {
         return (
-            <div>
-                <div>
+            <div id="orgContainer">
+                <div id="orgDiv">
                     <h1>Enter your organization name</h1>
-                    <CustomInputField placeholder='organization name' valueChanged={(value) => this.setState({name: value})}/>
-                    <CustomButton label='Submit name' onClick={this.submitName}/>
+                    <div id="orgInputField"><CustomInputField style={{minWidth: '500px'}} placeholder='organization name' valueChanged={(value) => this.setState({name: value})}/></div>
+                    <div id="orgInputBtn"><CustomButton label='Submit name' onClick={this.submitName}/></div>
                 </div>
             </div>
         );
