2a25025f70458ab86e1395d6cc43c43cc3fabc86
Daan Brocatus <daan.brocatus@outlook.com>
2020-01-10 14:47:48 +0100

    Make change color button functional

--- src/components/settings/Preferences.js
+++ src/components/settings/Preferences.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import './StyleSettings.css';
+import { NextHighlight, CurrentHighlight } from "../../ThemeProvider";
+import { inject, observer } from "mobx-react";
 
-class Preferences extends Component {
-  render() {
+const Preferences = inject('store')(observer(class Preferences extends Component {
+    cycleColor(){
+        NextHighlight();
+    }
+
+    render() {
     return(
         <div className="SettingsViewContainer">
             <p className="SettingsHeader">Preferences</p>
 
-            <div className="SettingsSubmitBtn" style={{minWidth: "210px"}}>Change colorscheme</div>
+            <div className="SettingsSubmitBtn" style={{minWidth: "210px"}} onClick={this.cycleColor}>Change colorscheme</div>
         </div>
     );
   }
 }
+));
 export default Preferences;
