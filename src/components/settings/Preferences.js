72ea99d528f5735e98c17e9b7326877b65e95956
Daan Brocatus <daan.brocatus@outlook.com>
2020-01-10 14:42:12 +0100

    Style settings

--- src/components/settings/Preferences.js
+++ src/components/settings/Preferences.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import './StyleSettings.css';
 
 class Preferences extends Component {
   render() {
-    return <div className="preferencesContainer">me awesome container</div>;
+    return(
+        <div className="SettingsViewContainer">
+            <p className="SettingsHeader">Preferences</p>
+
+            <div className="SettingsSubmitBtn" style={{minWidth: "700px"}}>Change colorscheme</div>
+        </div>
+    );
   }
 }
 export default Preferences;
